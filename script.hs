b71805f70f0b221c9f7389e0de276910b8662a82
Iori YONEJI <fivo.11235813@gmail.com>
2015-02-10 22:31:56 +0900

    initial commit

---
+++ script.hs
@@ -0,0 +1,36 @@
+-- #!/usr/bin/runghc
+import Control.Monad
+import Text.Pandoc
+import System.Directory
+import System.IO
+import System.Exit
+import Data.List
+import System.FilePath.Posix
+
+gettemplate :: IO(String)
+gettemplate =
+	getDirectoryContents "." >>=
+		(filterM doesFileExist) . (filter $ isSuffixOf ".template") >>=
+			mapM readFile >>= return . concat
+
+traversedocs :: IO [(FilePath, String)]
+traversedocs = do
+	a <- getDirectoryContents "." >>= (filterM doesFileExist) . (filter $ isSuffixOf ".md")
+	b <- mapM readFile a
+	return $ zip (map (\a -> replaceExtension a "html") a) b
+
+docconvert :: String -> (FilePath, String) -> (FilePath, String)
+docconvert t (f, s) = (f, md2html t s)
+
+md2html:: String -> String -> String
+md2html t = (writeHtmlString (writerconf t)) . readMarkdown def
+
+writerconf a = def {writerTemplate = a, writerStandalone = True}
+
+main::IO()
+main = do
+	fs <- traversedocs
+	t <- gettemplate
+	mapM_ (\(s,t) -> writeFile s t) $ map (docconvert t) fs
+--main = traversedocs >>= mapM (writehtml . md2html) >> exitWith ExitSuccess
+
